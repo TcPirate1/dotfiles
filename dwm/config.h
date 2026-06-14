1e8d35d6a97a353a73378c79049b8a2ac8384d7d
TC_arch_btw <88120195+TcPirate1@users.noreply.github.com>
2026-06-15 09:18:44 +1200

    Update the keybind to toggle between two tags to alt+tab

--- dwm/config.h
+++ dwm/config.h
@@ -3,12 +3,12 @@
 #include <X11/XF86keysym.h>
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int horizpadbar        = 2;
 static const int vertpadbar         = 0;
-static const int topbar             = 1;        /* 0 means bottom bar */
+static const int horizpadbar        = 0;
+static const int topbar             = 0;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Hack Nerd Font:size=13" ,"monospace:size=13" };
 static const char dmenufont[]       = "monospace:size=14";
 static const char col_gray1[]       = "#222222";
@@ -23,7 +23,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -38,7 +38,7 @@ static const Rule rules[] = {
 /* layout(s) */
 static const float mfact     = 0.50; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 static const int refreshrate = 120;  /* refresh rate (per second) for client move/resize */
 
@@ -64,15 +64,16 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-b", "-i", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-i", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[] = { "ghostty", NULL };
 static const char *firefox[] = { "firefox" , NULL };
 static const char *files[] = { "thunar" , NULL };
 static const char *obsidian[] = { "obsidian" , NULL };
 static const char *writer[] = { "libreoffice", "--writer", NULL };
-static const char *flameshot[] = { "flameshot", "gui", NULL }; // ctrl+a to capture entire screen, esc to leave any flameshot window
+static const char *flameshot[] = { "flameshot", "gui", NULL };
+// ctrl+a to capture entire screen, esc to leave any flameshot window
 static const char *flameshot_config[] = { "flameshot", "config", NULL };
-static const char *dwm_music_playlist[] = { "mpv", "--player-operation-mode=pseudo-gui", "--playlist=~/Music/Music/playlist.m3u", "--shuffle", NULL };
+static const char *dwm_music_playlist[] = { "mpv", "--player-operation-mode=pseudo-gui", "--playlist=~/Music/playlist.m3u", "--shuffle", NULL };
 static const char *qbittorrent[] = { "qbittorrent", "--no-splash", NULL};
 static const char *kiwix_desktop[] = { "kiwix-desktop", NULL };
 static const char *vivaldi[] = {"vivaldi", NULL};
@@ -102,7 +103,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
+	{ Mod1Mask,                     XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
 	// { MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	// { MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
